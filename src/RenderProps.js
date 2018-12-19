7c65266b8b12df96e064d8d5e9b02f3145448596
abecker <andrej@solekey.de>
2018-12-19 15:44:02 +0100

    Init

---
+++ src/RenderProps.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+
+class Cat extends React.Component {
+
+    render() {
+
+        const mouse = this.props.mouse;
+
+        return (
+            <img src="https://pbs.twimg.com/media/CsW0pmxUsAAuvEN.jpg:small"
+                 style={{
+                     position: 'absolute',
+                     left: mouse.x - 20,
+                     top: mouse.y - 20,
+                     width: '50px',
+                     height: '50px'
+                 }}
+            />
+        );
+    }
+}
+
+class Mouse extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+
+        this.state = {
+            x: 0,
+            y: 0
+        };
+    }
+
+    handleMouseMove(event) {
+        this.setState({
+            x: event.clientX,
+            y: event.clientY
+        });
+    }
+
+    render() {
+        return (
+            <div style={{ height: '100%' }}
+                 onMouseMove={this.handleMouseMove}
+            >
+
+                {/*
+                  Instead of providing a static representation of what <Mouse> renders,
+                  use the `render` prop to dynamically determine what to render.
+                */}
+                {
+                    this.props.rendermal(this.state)
+                }
+            </div>
+        );
+    }
+}
+
+class MouseTracker extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <h1>Move the mouse around!</h1>
+                <Mouse rendermal={
+                    mouse => (
+                        <Cat mouse={mouse}/>
+                    )
+                }
+                />
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+            <MouseTracker/>
+        );
+    }
+}
+
+
+export default App;
